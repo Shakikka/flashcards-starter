51fd1a9e3a1d81bf53f04a76ffb9328061c1dc0a
Joel Thomas <wilddroppings@gmail.com>
2021-02-02 20:05:44 -0700

    Add methods to Turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -32,9 +32,27 @@ describe('Turn', function() {
     expect(turn.returnGuess()).to.equal('object')
   })
 
+  it('should return the current Card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+    const turn = new Turn('object', card)
+    expect(turn.returnCard()).to.equal(card)
+  })
 
+  it('should evaluate if the guess is correct', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+    const turn1 = new Turn('object', card)
+    const turn2 = new Turn('array', card)
+    expect(turn1.evaluateGuess()).to.equal(true)
+    expect(turn2.evaluateGuess()).to.equal(false)
+  })
 
-
+  it('should return feedback if guess is correct or not', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+    const turn1 = new Turn('object', card)
+    const turn2 = new Turn('array', card)
+    expect(turn1.giveFeedback()).to.equal('correct!')
+    expect(turn2.giveFeedback()).to.equal('incorrect!')
+  })
 
 
 
