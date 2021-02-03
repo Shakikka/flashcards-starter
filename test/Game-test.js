26f0efba06943748d3dfaae45795f2d27b7c2164
Joel Thomas <wilddroppings@gmail.com>
2021-02-02 19:43:21 -0700

    Add basic turn class

---
+++ test/Game-test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Turn = require('../src/Turn')
+const Card = require('../src/Card')
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function')
+  });
+
+  it('should instantiate Card', function() {
+    const turn = new Turn();
+    expect(turn).to.be.instanceof(Turn)
+  })
+
+  it('should accept a guess', function() {
+    const turn = new Turn('Is it a horse?')
+    expect(turn.guess).to.equal('Is it a horse?')
+  })
+
+  it('should invoke an instance of Card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+    const turn = new Turn('object', card)
+    expect(turn.card).to.equal(card)
+
+  })
+
+
+
+
+
+
+
+});
