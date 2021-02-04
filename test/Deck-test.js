94e4cea7d907e02cee6d42eb631d18931b162053
Joel Thomas <wilddroppings@gmail.com>
2021-02-03 18:10:10 -0700

    Add functionality to Deck.js

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -14,15 +14,15 @@ describe('Deck', function() {
     expect(newDeck).to.be.instanceof(Deck);
   });
 
-  it.skip('should be able to accept an array of cards', function() {
+  it('should be able to accept an array of cards', function() {
     const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
     const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
-    const newDeck = new Deck([card1, card2, card3]);
-    expect(newDeck.cards[1][3]).to.equal('array');
-    expect(newDeck.cards).to.equal([card1, card2]);
+    const newDeck = new Deck([card1, card2]);
+    expect(newDeck.cards[1].correctAnswer).to.equal('array');
+    expect(newDeck.cards).to.deep.equal([card1, card2]);
   });
 
-  it.skip('should know how many cards are in the deck', function() {
+  it('should know how many cards are in the deck', function() {
     const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
     const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
     const newDeck = new Deck([card1, card2]);
